81b6a7c3998e48b353fed6429050a112424f7307
kiizerd <ajsewell13@gmail.com>
2021-09-30 21:13:57 -0500

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_01_005806) do
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
