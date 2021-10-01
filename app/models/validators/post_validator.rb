1b9a00a095bcbbfd0bab5b9feeedae669972667b
kiizerd <ajsewell13@gmail.com>
2021-09-30 21:39:35 -0500

    cleaned up post_validator

--- app/models/validators/post_validator.rb
+++ app/models/validators/post_validator.rb
@@ -1,18 +1,28 @@
 module Validators
   class PostValidator < ActiveModel::Validator
     def validate(record)
-      if record.body.blank? && record.link.blank?
-        record.errors.add :body, 'Post must either have a body or a link'
-        record.errors.add :link, 'Post must either have a body or a link'
-      end
+      body_or_link(record)
+      body_validation(record)
+      link_validation(record)
+    end
 
-      if record.body.blank? && record.link !~ URI::Parser.new.make_regexp(%w[http https])
-        record.errors.add :link, 'is invalid'
-      end
+    def body_or_link(record)
+      return unless record.body.blank? && record.link.blank?
 
-      if record.link.blank? && !record.body&.size&.between?(10, 750)
-        record.errors.add :body, 'must be between 10 and 750 chars'
-      end
+      record.errors.add :body, 'Post must either have a body or a link'
+      record.errors.add :link, 'Post must either have a body or a link'
+    end
+
+    def body_validation(record)
+      return unless record.body.blank? && record.link !~ URI::Parser.new.make_regexp(%w[http https])
+
+      record.errors.add :link, 'is invalid'
+    end
+
+    def link_validation(record)
+      return unless record.link.blank? && !record.body&.size&.between?(10, 750)
+
+      record.errors.add :body, 'must be between 10 and 750 chars'
     end
   end
 end
